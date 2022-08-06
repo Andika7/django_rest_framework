2f8fe0861d97522ef406663111cf9f7dc0b8647f
Andika Pratama <apratama918@gmail.com>
2022-08-06 15:44:52 +0700

    add rest api

--- my_rest_api/settings.py
+++ my_rest_api/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # this line for app
+    'rest_framework',
+    'product_api',
 ]
 
 MIDDLEWARE = [
--- product_api/views.py
+++ product_api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
 # Create your views here.
+from product_api.models import *
+from product_api.serializers import *
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows category to be viewed or edited.
+    """
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    # permission_classes = [permissions.IsAuthenticated]
+
+
+class ItemViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows items to be viewed or edited.
+    """
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+    # permission_classes = [permissions.IsAuthenticated]
